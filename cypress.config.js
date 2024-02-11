b7c4bde4ed0999917537afb7ca066192ef770a39
Jessica Melo <jessicamelo2015@gmail.com>
2024-02-11 11:24:56 -0300

    adicionando arquivo de dados com fixture

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:'http://lojaebac.ebaconline.art.br/#',
+   /// baseUrl:'http://lojaebac.ebaconline.art.br/#',
+   baseUrl: 'http://lojaebac.ebaconline.art.br/',
     specPattern:'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,8 +1,9 @@
 ///<reference types="cypress"/>
+const perfil= require('../../fixtures/perfil.json')
 
      describe('Funcionalidade: Login',() => {
         beforeEach(() => {
-            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.visit('minha-conta')
         });
        /// afterEach(() => {
            ///cy.screenshot()
@@ -34,7 +35,25 @@
             cy.get('.woocommerce-error').should('exist')
         });
 
-        
+        it('Deve fazer login com sucesso - Usando massa de dados', () => {
+            cy.get('#username').type(perfil.usuario)
+            cy.get('#password').type(perfil.senha)
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, jessicamelo2015 (não é jessicamelo2015? Sair)')
+
+            
+        });
+        it.only('Deve fazer login com sucesso - Usando fixture', () => {
+           cy.fixture('perfil').then(dados =>{
+
+            cy.get('#username').type(dados.usuario,{log:false})
+            cy.get('#password').type(dados.senha,{log:false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, jessicamelo2015 (não é jessicamelo2015? Sair)')
+           })
+            
+        });
 
         
 
